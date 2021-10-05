b1b7f6a559dbcb168f893e087d2d7a78b4da7905
mdmathewdc <mathewdony007@gmail.com>
2021-10-05 16:49:17 +1100

    Added a button

--- syncit/pages/index.js
+++ syncit/pages/index.js
@@ -1,8 +1,11 @@
-import { Heading, Page } from "@shopify/polaris";
+import { Heading, Page, Button } from "@shopify/polaris";
 
 const Index = () => (
   <Page>
     <Heading> I changed the heading of the App</Heading>
+
+    <Button ariaExpanded={false}>Add product</Button>
+
   </Page>
 );
 
