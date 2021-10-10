cb7a32d9bd27180156b407fe808cb8a67fdc51b1
mdmathewdc <mathewdony007@gmail.com>
2021-10-10 18:51:00 +1100

    Added a side navbar

--- syncit/pages/index.js
+++ syncit/pages/index.js
@@ -1,11 +1,34 @@
-import { Heading, Page, Button } from "@shopify/polaris";
+import { Heading, Page, Button, Navigation } from "@shopify/polaris";
+import {HomeMajor, OrdersMajor, ProductsMajor} from '@shopify/polaris-icons';
+
 
 const Index = () => (
   <Page>
     <Heading> I changed the heading of the App</Heading>
+    <Navigation location="/">
+      <Navigation.Section
+        items={[
+          {
+            url: "/path/to/place",
+            label: "Home",
+            icon: HomeMajor,
+          },
+          {
+            url: "/path/to/place",
+            label: "Orders",
+            icon: OrdersMajor,
+            badge: "15",
+          },
+          {
+            url: "/path/to/place",
+            label: "Products",
+            icon: ProductsMajor,
+          },
+        ]}
+      />
+    </Navigation>
 
     <Button ariaExpanded={false}>Add product</Button>
-
   </Page>
 );
 
