4f438af29e0934406ada7e5e3b632cb17426df6c
mdmathewdc <mathewdony007@gmail.com>
2021-10-18 12:58:23 +1100

    Removed a line

--- syncit/server/server.js
+++ syncit/server/server.js
@@ -75,8 +75,6 @@ app.prepare().then(async () => {
 
   router.get("/home", async (ctx) => {
     ctx.response.body = JSON.stringify("Home route...Node JS Server");
-
-    connector();
     const uri = "mongodb+srv://syncit_admin:syncit_password@cluster0.iuory.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
     const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
     client.connect(err => {
